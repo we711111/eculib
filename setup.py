9473ebb9377f42e1e86fd862a31cc4be16ffa837
we711111 <65511884+we711111@users.noreply.github.com>
2020-05-21 10:55:52 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='eculib',
     version='1.0.19',
     description='A library for K-line based ECU communication',
-    url='https://github.com/darkheromx/HDlib',
+    url='https://github.com/we711111/eculib',
     author='Sabsteef',
     author_email='',
     license='GPL-3',

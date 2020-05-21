e22716011f696f831f9455f570790a2670a6b750
we711111 <65511884+we711111@users.noreply.github.com>
2020-05-22 05:43:11 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,6 @@ setup(
     name='eculib',
     version='1.0.37',
     description='A library for K-line based ECU communication',
-    url='https://github.com/we711111/eculib',
-    author='Ryan M. Hope',
-    author_email='chadchai.ping.cs@hotmail.com',
-    license='GPL-3',
     packages=['eculib'],
     entry_points={
         'console_scripts': ['eculib=eculib.__main__:Main'],

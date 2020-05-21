698572f90951d1a7b7819c3771859485ece0edee
we711111 <65511884+we711111@users.noreply.github.com>
2020-05-21 11:27:25 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='eculib',
-    version='1.0.9',
+    version='1.0.37',
     description='A library for K-line based ECU communication',
     packages=['eculib'],
     entry_points={

74d6e55b3cf13a515668d0089ff49c81900d0859
we711111 <65511884+we711111@users.noreply.github.com>
2020-05-22 06:05:14 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name='eculib',
     version='1.0.37',
     description='A library for K-line based ECU communication',
+    url='https://github.com/darkheromx/HDlib',
+    author='Ryan M. Hope',
+    author_email='chadchai.ping.cs@hotmail.com',
+    license='GPL-3',
     packages=['eculib'],
     entry_points={
         'console_scripts': ['eculib=eculib.__main__:Main'],
